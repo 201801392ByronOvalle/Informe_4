ed459ab1c77f26eb1d35978671371278e8092792
201807002-Sophia-Salguero <2787973850101@ingenieria.usac.edu.gt>
2022-09-29 01:44:44 -0600

    loging

--- fronted/src/app/app-routing.module.ts
+++ fronted/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+import { componentFactoryName } from '@angular/compiler';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import{ LoginFormComponent } from './components/login-form/login-form.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo: '/inicio',
+    pathMatch: 'full' 
+  },
+  {
+    path: 'inicio/registro',
+    component: LoginFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
